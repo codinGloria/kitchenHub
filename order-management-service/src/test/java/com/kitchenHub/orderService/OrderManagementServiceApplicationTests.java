0bbd8a52a030cf53e6f75bf79015370adbb68535
codinGloria <codingloria@gmail.com>
2025-01-29 20:07:22 -0300

    ✨ feat: starter pack

---
+++ order-management-service/src/test/java/com/kitchenHub/orderService/OrderManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kitchenHub.orderService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
